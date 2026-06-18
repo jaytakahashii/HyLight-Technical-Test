58604760ac7834ff504d7ca2d210846dec8e970f
Jay <qsakura14555@gmail.com>
2026-06-18 20:03:55 +0200

    chore: delete a unnecessary comment

--- hylight-map-app/eslint.config.mjs
+++ hylight-map-app/eslint.config.mjs
@@ -1,6 +1,6 @@
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
-import prettierConfig from 'eslint-config-prettier'; // 追加
+import prettierConfig from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
 const eslintConfig = tseslint.config(
