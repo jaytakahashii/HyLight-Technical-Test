a51ec720ef225fcc219c178b9416352f3630b26f
Jay <qsakura14555@gmail.com>
2026-06-19 21:37:08 +0200

    fix: add production env

--- hylight-map-app/next.config.ts
+++ hylight-map-app/next.config.ts
@@ -6,7 +6,13 @@ const nextConfig = {
         protocol: 'http',
         hostname: '127.0.0.1',
         port: '54321',
-        pathname: '/storage/v1/object/public/**', // SupabaseのストレージURLを指定
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pkxtzoonwcnxtivrfdgn.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
       },
     ],
   },
